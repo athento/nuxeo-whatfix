ef182fd26b4e931d609e7bc2803f6f00640fc88c
Angajime <tarazaky@alu.uma.es>
2015-04-16 13:06:49 +0200

    .gitignore fixed to include *.war

--- src/main/resources/web/nuxeo.war/scripts/whatfix.js
+++ src/main/resources/web/nuxeo.war/scripts/whatfix.js
@@ -1,26 +1,3 @@
-/*_wfx_flow='e645fcc0-e351-11e4-9db2-448a5b5dd1ba'; window._wfx_live && _wfx_live();
-
-_wfx_flow='780c6100-e355-11e4-9448-448a5b5dd5b8'; window._wfx_live && _wfx_live();
-
-_wfx_flow='2aa0b580-e358-11e4-9448-448a5b5dd5b8'; window._wfx_live && _wfx_live();
-
-_wfx_flow='b5f292e0-e35b-11e4-9db2-448a5b5dd1ba'; window._wfx_live && _wfx_live();
-
-_wfx_flow='8894ff80-e35c-11e4-9db2-448a5b5dd1ba'; window._wfx_live && _wfx_live();
-
-_wfx_flow='1fe18110-e362-11e4-9448-448a5b5dd5b8'; window._wfx_live && _wfx_live();
-
-_wfx_flow='d3df0d10-e365-11e4-9db2-448a5b5dd1ba'; window._wfx_live && _wfx_live();
-
-_wfx_flow='41bf7960-e36f-11e4-9448-448a5b5dd5b8'; window._wfx_live && _wfx_live();
-*/
-
-/*
-var r = document.createElement("script");
-r.language='javascript';
-r.html = "";
-jQuery("head").append(r);
-*/
 jQuery("head").append("<script>window._wfx_widget={\"color\":\"#ED9121\", \"position\":\"rtm\", \"host\":\"cloud.athento.com\"};</script>")
 
 var s = document.createElement("script");
